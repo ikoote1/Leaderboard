947bc456f7ddb817934384b7a96e45a41f2333a7
ikoote1 <ikootepreim@gmail.com>
2023-03-07 16:32:55 +0300

    Added webpack files

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./style.css";
+console.log("Hello webpack!");
+const fancyFunc = () => {
+    return [1, 2];
+  };
+  
+  const [a, b] = fancyFunc();
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ]
+  },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: path.resolve(__dirname, "src", "index.html")
+        })
+      ]
+  };
