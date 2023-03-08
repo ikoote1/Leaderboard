8932ca72b0654e5e5a762c62d7087d84568acaab
ikoote1 <ikootepreim@gmail.com>
2023-03-08 10:09:56 +0300

    Fixed linter errors

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  mode:'development',
+  mode: 'development',
   module: {
     rules: [
       {
